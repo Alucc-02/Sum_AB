3aff4d5ed36c8f6e3f0013e236ff33b767a7b9ac
Alucc-02 <lucca.slazzari@gmail.com>
2024-03-14 10:01:04 -0300

    Atividade simples para fazer testes com a função "sum()"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("teste de envio");
+        Scanner t = new Scanner(System.in);
+        System.out.print("Valor de 'a' = ");
+        int a = t.nextInt();
+        System.out.print("Valor de 'b' = ");
+        int b = t.nextInt();
+        System.out.println("A soma de 'a' + 'b' = "+ Integer.sum(a,b));
+
+
     }
 }
